82a416b413f11dd8f400a1cc65f719789b78d461
Jakub969 <jakub.hrubizna@gmail.com>
2026-02-25 10:04:46 +0100

    uprava pre zobrazenie progresu

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import shutil
+
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
 from uuid import uuid4
@@ -27,10 +29,20 @@ async def upload_video(file: UploadFile = File(...)):
     }
 
 
-@app.get("/result/{job_id}")
-def get_result(job_id: str):
-    file_path = JOBS_DIR / job_id / "prediction_output.glb"
-    return FileResponse(file_path, media_type="model/gltf-binary")
+@app.get("/result/{job_id}/{quality}")
+def get_result(job_id: str, quality: str):
+
+    if quality == "sparse":
+        path = JOBS_DIR / job_id / "sparse" / "sparse_prediction.glb"
+    else:
+        path = JOBS_DIR / job_id / "dense" / "dense_prediction.glb"
+
+    return FileResponse(path, media_type="model/gltf-binary")
+
+@app.delete("/job/{job_id}")
+def delete_job(job_id: str):
+    shutil.rmtree(JOBS_DIR / job_id, ignore_errors=True)
+    return {"deleted": True}
 
 @app.get("/status/{task_id}")
 def check_status(task_id: str):
@@ -40,4 +52,25 @@ def check_status(task_id: str):
         "state": task.state,
         "result": task.result
     }
+
+from celery.result import AsyncResult
+
+@app.get("/progress/{task_id}")
+def get_progress(task_id: str):
+
+    task = AsyncResult(task_id)
+
+    if task.state == "PROGRESS":
+        return {
+            "state": "PROGRESS",
+            "progress": task.info["progress"]
+        }
+
+    if task.state == "SUCCESS":
+        return {
+            "state": "SUCCESS",
+            "progress": 100
+        }
+
+    return {"state": task.state}
 ##uvicorn main:app --host 0.0.0.0 --port 8000
--- app/tasks.py
+++ app/tasks.py
@@ -11,7 +11,16 @@ from DBSCAN import filter_point_cloud
 
 
 @celery.task(name="tasks.process_video")
-def process_video(job_id: str, video_path_str: str, quality):
+def process_video(job_id: str, video_path_str: str, quality: str):
+    total_progress = 0
+
+    def update(p):
+        nonlocal total_progress
+        total_progress += p
+        celery.update_state(
+            state="PROGRESS",
+            meta={"progress": total_progress}
+        )
 
     job_dir = JOBS_DIR / job_id
     image_dir = job_dir / "images"
@@ -25,68 +34,44 @@ def process_video(job_id: str, video_path_str: str, quality):
 
     video_path = Path(video_path_str)
 
-    # -------------------------
-    # 1) Frame extraction
-    # -------------------------
-    subprocess.run([
-        FFMPEG_PATH,
-        "-i", str(video_path),
-        "-qscale:v", "2",
-        str(image_dir / "frame_%06d.jpg")
-    ], check=True)
+    sparse_glb = sparse_dir / "sparse_prediction.glb"
+    dense_glb = dense_dir / "dense_prediction.glb"
 
-    # -------------------------
-    # 2) Feature extraction (GPU)
-    # -------------------------
-    pycolmap.extract_features(
-        database_path=db_path,
-        image_path=image_dir,
-        camera_mode=pycolmap.CameraMode.SINGLE,
-        device=pycolmap.Device.cuda
-    )
+    if not sparse_glb.exists():
 
-    # -------------------------
-    # 3) Sequential matching
-    # -------------------------
-    pycolmap.match_sequential(
-        database_path=db_path,
-        device=pycolmap.Device.cuda,
-    )
+        if not any(image_dir.glob("*.jpg")):
+            subprocess.run([
+                FFMPEG_PATH,
+                "-i", str(video_path),
+                "-qscale:v", "2",
+                str(image_dir / "frame_%06d.jpg")
+            ], check=True)
+            update(5)
+        if not db_path.exists():
+            pycolmap.extract_features(
+                database_path=db_path,
+                image_path=image_dir,
+                camera_mode=pycolmap.CameraMode.SINGLE,
+                device=pycolmap.Device.cuda
+            )
+            update(20)
+            pycolmap.match_sequential(
+                database_path=db_path,
+                device=pycolmap.Device.cuda,
+            )
+            update(15)
+        if not (sparse_dir / "0").exists():
+            pycolmap.incremental_mapping(
+                database_path=db_path,
+                image_path=image_dir,
+                output_path=sparse_dir
+            )
+            update(25)
+        model_path = sorted(sparse_dir.glob("*"))[0]
 
-    # -------------------------
-    # 4) Sparse mapping
-    # -------------------------
-    pycolmap.incremental_mapping(
-        database_path=db_path,
-        image_path=image_dir,
-        output_path=sparse_dir
-    )
+        sparse_ply = sparse_dir / "sparse_points.ply"
 
-    model_path = sparse_dir / "0"
-
-    # -------------------------
-    # 5) Dense reconstruction
-    # -------------------------
-    if quality == "dense":
-        pycolmap.undistort_images(
-            output_path=dense_dir,
-            input_path=model_path,
-            image_path=image_dir
-        )
-
-        pycolmap.patch_match_stereo(
-            workspace_path=dense_dir
-        )
-
-        pycolmap.stereo_fusion(
-            output_path=dense_dir / "fused.ply",
-            workspace_path=dense_dir
-        )
-        input_ply = dense_dir / "fused.ply"
-    else:
-        sparse_ply = sparse_dir / "points3D.ply"
-
-        reconstruction = pycolmap.Reconstruction(sparse_dir)
+        reconstruction = pycolmap.Reconstruction(model_path)
 
         points = []
         colors = []
@@ -99,21 +84,72 @@ def process_video(job_id: str, video_path_str: str, quality):
         pcd.points = o3d.utility.Vector3dVector(np.array(points))
         pcd.colors = o3d.utility.Vector3dVector(np.array(colors))
         o3d.io.write_point_cloud(sparse_ply, pcd)
-        input_ply = sparse_ply
-    output_ply = job_dir / "filtered_scene.ply"
-    filter_point_cloud(str(input_ply), str(output_ply))
-    prediction_ply = job_dir / "prediction_output.ply"
 
-    subprocess.run([
-        "docker", "run", "--rm",
-        "--gpus", "all",
-        "-v", f"{job_dir}:/workspace/job",
-        "pointnet2_inference:latest",
-        "python", "full_inference.py",
-        "/workspace/job/filtered_scene.ply",
-        "/workspace/job/prediction_output.ply",
-        "/workspace/best_pointnet2_model.pth"
-    ], check=True)
+        filtered_sparse = sparse_dir / "filtered_sparse.ply"
+        filter_point_cloud(str(sparse_ply), str(filtered_sparse))
+        update(5)
 
-    glb_path = job_dir / "prediction_output.glb"
-    convert_ply_to_glb(input_ply, glb_path)
+        subprocess.run([
+            "docker", "run", "--rm",
+            "--gpus", "all",
+            "-v", f"{job_dir}:/workspace/job",
+            "pointnet2_inference:latest",
+            "python", "full_inference.py",
+            "/workspace/job/sparse/filtered_sparse.ply",
+            "/workspace/job/sparse/prediction_sparse.ply",
+            "/workspace/best_pointnet2_model.pth"
+        ], check=True)
+        update(20)
+        convert_ply_to_glb(
+            sparse_dir / "prediction_sparse.ply",
+            sparse_glb
+        )
+        update(10)
+    if quality == "dense" and not dense_glb.exists():
+        total_progress = 0
+
+        model_path = sorted(sparse_dir.glob("*"))[0]
+
+        pycolmap.undistort_images(
+            output_path=dense_dir,
+            input_path=model_path,
+            image_path=image_dir
+        )
+        update(10)
+
+        pycolmap.patch_match_stereo(
+            workspace_path=dense_dir
+        )
+        update(40)
+
+        pycolmap.stereo_fusion(
+            output_path=dense_dir / "fused.ply",
+            workspace_path=dense_dir
+        )
+        update(20)
+
+        filtered_dense = dense_dir / "filtered_dense.ply"
+        filter_point_cloud(
+            str(dense_dir / "fused.ply"),
+            str(filtered_dense)
+        )
+        update(10)
+
+        subprocess.run([
+            "docker", "run", "--rm",
+            "--gpus", "all",
+            "-v", f"{job_dir}:/workspace/job",
+            "pointnet2_inference:latest",
+            "python", "full_inference.py",
+            "/workspace/job/dense/filtered_dense.ply",
+            "/workspace/job/dense/prediction_dense.ply",
+            "/workspace/best_pointnet2_model.pth"
+        ], check=True)
+        update(10)
+        convert_ply_to_glb(
+            dense_dir / "prediction_dense.ply",
+            dense_glb
+        )
+        update(10)
+
+    return {"status": "done"}
